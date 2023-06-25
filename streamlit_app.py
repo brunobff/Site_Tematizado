062eec89aad178ba6c6d5b580e8fe69d3eeb3064
Bruno Felipe <61852563+BFFgit@users.noreply.github.com>
2023-06-25 12:58:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ arquivo = 'https://raw.githubusercontent.com/BFFgit/Sistema_Avaliacao_Criticas/m
 
 data = load_data()
 
-st.markdown("<h1 style='text-align: center; color: red;'>Análise de Sentimento de Críticas de Produtos</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: blue;'>Análise de Sentimento de Críticas de Produtos</h1>", unsafe_allow_html=True)
 
 st.header("Dataset de Críticas da Amazon")
 st.write(data)
